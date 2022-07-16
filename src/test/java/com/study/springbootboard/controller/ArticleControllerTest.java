0f563bbaf78143d5ffca99f20df228cbbf8ec248
skskdmsdl <skskdmsdl@gmail.com>
2022-07-16 18:58:04 +0900

    #16 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 80fb8bc 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고 미리 테스트 메소드 업데이트

--- src/test/java/com/study/springbootboard/controller/ArticleControllerTest.java
+++ src/test/java/com/study/springbootboard/controller/ArticleControllerTest.java
@@ -47,7 +47,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -63,7 +63,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -76,7 +76,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 
