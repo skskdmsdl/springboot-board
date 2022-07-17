260cbc9d32cad560a73062cb2acf2282589be3ee
skskdmsdl <skskdmsdl@gmail.com>
2022-07-17 19:01:49 +0900

    #16 - thymeleaf 3 decoupled logic 설정
    java code 로 thymeleaf 3 decoupled logic 을 사용자 application property 로 제어할 수 있게 설정 추가
    
    * https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

--- src/main/java/com/study/springbootboard/SpringbootBoardApplication.java
+++ src/main/java/com/study/springbootboard/SpringbootBoardApplication.java
@@ -2,7 +2,9 @@ package com.study.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class SpringbootBoardApplication {
 
