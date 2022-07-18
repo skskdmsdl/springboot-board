c87567eed8e592bfb4dd6fad9efea9f74a1db948
skskdmsdl <skskdmsdl@gmail.com>
2022-07-18 11:34:11 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해 준 `/login` 컨트롤러 테스트.
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@import` 추가

--- src/test/java/com/study/springbootboard/controller/ArticleControllerTest.java
+++ src/test/java/com/study/springbootboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.study.springbootboard.controller;
 
+import com.study.springbootboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class) // WebMvcTest는 모든 컨트롤러를 다 읽기 때문에 테스트 대상 컨트롤러만 빈으로 읽음
 class ArticleControllerTest {
 
