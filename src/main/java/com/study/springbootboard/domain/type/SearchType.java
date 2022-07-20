45271b2d724829e1222be4e605cdcc49b64b1486
skskdmsdl <skskdmsdl@gmail.com>
2022-07-20 09:47:49 +0900

    #20 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

---
+++ src/main/java/com/study/springbootboard/domain/type/SearchType.java
@@ -0,0 +1,5 @@
+package com.study.springbootboard.domain.type;
+
+public enum SearchType {
+    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+}
---
+++ src/main/java/com/study/springbootboard/service/ArticleService.java
@@ -0,0 +1,39 @@
+package com.study.springbootboard.service;
+
+import com.study.springbootboard.domain.type.SearchType;
+import com.study.springbootboard.dto.ArticleDto;
+import com.study.springbootboard.dto.ArticleUpdateDto;
+import com.study.springbootboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
