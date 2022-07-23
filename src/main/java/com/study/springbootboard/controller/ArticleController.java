cf2ac7195238fbe3b1df62edb897cc86b94848ff
skskdmsdl <skskdmsdl@gmail.com>
2022-07-23 14:07:44 +0900

    #25 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 `Page` 클래스를 이용하기 때문에 하는 김에 나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/com/study/springbootboard/controller/ArticleController.java
+++ src/main/java/com/study/springbootboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.study.springbootboard.domain.type.SearchType;
 import com.study.springbootboard.dto.response.ArticleResponse;
 import com.study.springbootboard.dto.response.ArticleWithCommentsResponse;
 import com.study.springbootboard.service.ArticleService;
+import com.study.springbootboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -23,6 +25,7 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -31,7 +34,11 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
