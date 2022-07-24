474f9e67758107ff6902b208d0b697db290b3e05
skskdmsdl <skskdmsdl@gmail.com>
2022-07-24 15:46:55 +0900

    #25 - 게시글 페이지네이션 구현
    `이전 글`, `다음 글`만 구현하므로 게시판에 비해 단순하지만
    첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서
    타임리프 문법이 복잡해짐
    
    마지막 글을 판단하기 위해 총 글 개수가 필요해졌고
    이를 위해 count 쿼리 사용, 서비스 로직 추가

--- src/main/java/com/study/springbootboard/service/ArticleService.java
+++ src/main/java/com/study/springbootboard/service/ArticleService.java
@@ -61,4 +61,8 @@ public class ArticleService {
     public void deleteArticle(long articleId) {
         articleRepository.deleteById(articleId);
     }
+
+    public long getArticleCount() {
+        return articleRepository.count();
+    }
 }
