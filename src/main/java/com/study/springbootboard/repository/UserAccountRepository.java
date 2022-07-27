64d6267a87d7d4f733ec37a2743be80f87a29575
skskdmsdl <skskdmsdl@gmail.com>
2022-07-27 09:23:35 +0900

    #37 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/study/springbootboard/repository/UserAccountRepository.java
+++ src/main/java/com/study/springbootboard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.study.springbootboard.repository;
 import com.study.springbootboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
