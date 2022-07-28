2cfe46188ee3ef756f90841ff8c831750fb60cde
skskdmsdl <skskdmsdl@gmail.com>
2022-07-28 09:19:59 +0900

    #44 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

---
+++ src/main/java/com/study/springbootboard/controller/ArticleCommentController.java
@@ -0,0 +1,37 @@
+package com.study.springbootboard.controller;
+
+import com.study.springbootboard.dto.UserAccountDto;
+import com.study.springbootboard.dto.request.ArticleCommentRequest;
+import com.study.springbootboard.service.ArticleCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+@Controller
+public class ArticleCommentController {
+
+    private final ArticleCommentService articleCommentService;
+
+    @PostMapping ("/new")
+    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
+        // TODO: 인증 정보를 넣어줘야 한다.
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
+                "uno", "pw", "uno@mail.com", null, null
+        )));
+
+
+        return "redirect:/articles/" + articleCommentRequest.articleId();
+    }
+
+    @PostMapping ("/{commentId}/delete")
+    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
+        articleCommentService.deleteArticleComment(commentId);
+
+        return "redirect:/articles/" + articleId;
+    }
+
+}
--- src/test/java/com/study/springbootboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/study/springbootboard/service/ArticleCommentServiceTest.java
@@ -7,6 +7,7 @@ import com.study.springbootboard.dto.ArticleCommentDto;
 import com.study.springbootboard.dto.UserAccountDto;
 import com.study.springbootboard.repository.ArticleCommentRepository;
 import com.study.springbootboard.repository.ArticleRepository;
+import com.study.springbootboard.repository.UserAccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,6 +32,8 @@ class ArticleCommentServiceTest {
 
     @Mock private ArticleRepository articleRepository;
     @Mock private ArticleCommentRepository articleCommentRepository;
+    @Mock private UserAccountRepository userAccountRepository;
+
 
     @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
@@ -56,6 +59,7 @@ class ArticleCommentServiceTest {
         // Given
         ArticleCommentDto dto = createArticleCommentDto("댓글");
         given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
         given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
         // When
@@ -63,6 +67,7 @@ class ArticleCommentServiceTest {
 
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
@@ -78,6 +83,7 @@ class ArticleCommentServiceTest {
 
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).shouldHaveNoInteractions();
         then(articleCommentRepository).shouldHaveNoInteractions();
     }
 
