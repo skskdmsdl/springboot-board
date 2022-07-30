73e75c1a578091634f1535272e13fd734737b8ff
skskdmsdl <skskdmsdl@gmail.com>
2022-07-30 17:35:23 +0900

    #46 - 패스워드 인코더가 인식할 수 있게 패스워드 정보 수정
    앞머리에 `{encoding-info}`와 같이 암호화 정보를 넣어줘야
    패스워드 인코더가 읽을 수 있다.
    이에 패스워드 데이터를 알맞게 수정함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('nana', 'asdf1234', 'nana', 'nana@mail.com', 'I am nana.', now(), 'nana', now(), 'nana')
+('nana', '{noop}asdf1234', 'nana', 'nana@mail.com', 'I am nana.', now(), 'nana', now(), 'nana')
 ;
 
 -- 123 게시글
